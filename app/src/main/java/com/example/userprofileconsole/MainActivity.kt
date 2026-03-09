d7aa2ea77fd5702825592eeb0bb0d5b96c18faed
Sirib <sirib219@gmail.com>
2026-03-09 16:57:20 +0530

    Updated MainActivity to use BusinessCardLayout

--- app/src/main/java/com/example/userprofileconsole/MainActivity.kt
+++ app/src/main/java/com/example/userprofileconsole/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.userprofileconsole
-
+import com.example.userprofileconsole.BusinessCardLayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,10 +15,9 @@ class MainActivity : ComponentActivity() {
             UserProfileConsoleTheme {
 
                 Surface {
-
-                    ProfileLayout()
-
+                    BusinessCardLayout()
                 }
+
             }
         }
     }
