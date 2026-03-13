33f33a0a63239f9b4f833f97737ad2f756452ee6
Sirib <sirib219@gmail.com>
2026-03-13 14:01:01 +0530

    Session 9: Advanced Inventory Manager with search, filters, sorting and add item dialog updated

--- app/src/main/java/com/example/userprofileconsole/NullSafetyExample.kts
+++ app/src/main/java/com/example/userprofileconsole/NullSafetyExample.kts
@@ -4,16 +4,29 @@ data class Product(
     val category: String?
 )
 
+println("🛒 Product List")
+
 val products = listOf(
     Product("Laptop",70000.0,"Electronics"),
     Product(null,150.0,"Accessories"),
     Product("Notebook",null,null)
 )
 
-products.forEach {
-    val name = it.name ?: "Unknown Product"
-    val price = it.price ?: 0.0
-    val category = it.category ?: "General"
+products.forEach { product ->
 
-    println("$name - ₹$price - $category")
+    val name = product.name ?: "Unknown Product"
+    val price = product.price ?: 0.0
+    val category = product.category ?: "General"
+
+    println("Product: $name")
+    println("Price: ₹$price")
+    println("Category: $category")
+
+    if(price > 10000){
+        println("Category Type: Premium Product")
+    } else {
+        println("Category Type: Budget Product")
+    }
+
+    println("------------------------")
 }
